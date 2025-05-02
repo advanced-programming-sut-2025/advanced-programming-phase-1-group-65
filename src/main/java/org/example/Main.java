9a768bcb8443f9845e2ecf02250e058394f25d54
Arya6785 <aria.hoseini2018@gmail.com>
2025-05-02 21:08:46 +0330

    map template half done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,19 @@
 package org.example;
 
 import org.example.Models.App;
+import org.example.Models.Tile;
 import org.example.Views.AppView;
+import org.example.Models.Map;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Map m = new Map();
+        ArrayList<ArrayList<Tile>> map = m.buildMap();
+        m.printMap(map);
+
         Scanner scanner = new Scanner(System.in);
 
         try {
